416ecd3b479db1da0db59b12c114aada94808db7
kd4293_olive <sumeetb@olivetheory.com>
2019-04-18 12:28:56 +0530

    heroku upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
+import ToDo from './models/todo.model';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tacto-todo-app';
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  public newTodo: ToDo = new ToDo()
+
+  todosList: ToDo[] = [];
+  editTodos: ToDo[] = [];
+  title = 'app';
+
+  ngOnInit(): void {
+    
+  }
+
+
+  create() {
+        this.todosList.push(this.newTodo);
+        this.newTodo = new ToDo()
+  }
+
+  editTodo(todo: ToDo) {
+    console.log(todo)
+    if(this.todosList.includes(todo)){
+      if(!this.editTodos.includes(todo)){
+        this.editTodos.push(todo)
+      }else{
+        this.editTodos.splice(this.editTodos.indexOf(todo), 1)
+       
+      }
+    }
+  }
+
+  doneTodo(todo:ToDo){
+    todo.status = 'Done'
+    
+  }
+
+  submitTodo(event, todo:ToDo){
+    if(event.keyCode ==13){
+      this.editTodo(todo)
+    }
+  }
+
+  deleteTodo(todo: ToDo) {
+    this.todosList.splice(this.todosList.indexOf(todo), 1);
+  }
+
 }
---
+++ src/app/models/todo.model.ts
@@ -0,0 +1,13 @@
+class ToDo {
+    _id:string;
+    task: string;
+    status: string;
+
+    constructor(
+    ){
+        this.task = ""
+        this.status = ""
+    }
+}
+
+export default ToDo;
